25861eb4aa602949ee1c22ac98b6cdd51f6b8e61
PKNaveen <88645434+PKNaveen@users.noreply.github.com>
2024-06-17 08:44:05 +0530

    Update layout.tsx
    changed mobile nav layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -19,10 +19,10 @@ export default async function RootLayout({
 
       <div className="flex size-full flex-col">
         <div className="root-layout">
-          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
           <div>
             <MobileNav user={loggedIn} />
           </div>
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
         </div>
         {children}
       </div>
