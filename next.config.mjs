ef26cbea03e9ecab4784f5b855ed8b31fdf699e2
PKNaveen <88645434+PKNaveen@users.noreply.github.com>
2024-06-17 08:00:38 +0530

    final

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ export default withSentryConfig(nextConfig, {
 
 // Suppresses source map uploading logs during build
 silent: true,
-org: "jsm-x9",
+org: "naveens-org",
 project: "javascript-nextjs",
 }, {
 // For all available options, see:
